603d74ff29f19d27d36eb657d455fe3b965350b1
Michael Thessel <michael.thessel@gmx.de>
2019-11-24 13:29:59 -0800

    Added automatic album advancing

--- stoerbert.ino
+++ stoerbert.ino
@@ -13,6 +13,7 @@ TODO:
  * Album select god mode
 * Volume pot handling
 * Sleep mode
+* Save current track on EEPROM
 
 Reseach if headphone jack is possible
 
@@ -52,6 +53,7 @@ char *album[MAX_TRACKS]; // Album track buffer
 char currentAlbum[] = "k01"; // Currently selected album
 uint8_t currentAlbumTrackCount = 0; // Track count for currently selected album
 uint8_t currentTrack = 0; // Current track
+bool isPlaying = false; // Whether or not there is currently an album playing
 uint8_t gmflag = 0; // God mode flag
 
 void setup() {
@@ -63,10 +65,9 @@ void setup() {
         DPRINTLNF("Couldn't find VS1053");
         while (1);
     }
-
     DPRINTLNF("VS1053 found");
 
-     if (!SD.begin(CARDCS)) {
+    if (!SD.begin(CARDCS)) {
         DPRINTLNF("SD failed, or not present");
         while (1);
     }
@@ -78,77 +79,10 @@ void setup() {
 
 void loop() {
     #ifdef DEBUG
-    if (Serial.available()) {
-        char c = Serial.read();
-
-        switch (c) {
-            // Start playing
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-                DPRINTF("Received Command: PLAY ");
-                DPRINTLN(c);
-                currentAlbum[2] = c;
-                detectGodMode(c);
-                playAlbum();
-                break;
-
-            // GOD mode !!!
-            case 'g':
-                DPRINTLNF("Received Command: God Mode");
-                setGodMode();
-                break;
-
-            // Toggle play/pause
-            case 'p':
-                DPRINTLNF("Received Command: TOGGLE PLAY/PAUSE");
-                playTogglePause();
-                break;
-
-            // Next track
-            case 'f':
-                DPRINTLNF("Received Command: NEXT TRACK");
-                playNextTrack();
-                break;
-
-            // Previous track
-            case 'b':
-                DPRINTLNF("Received Command: PREVIOUS TRACK");
-                playPreviousTrack();
-                break;
-
-            // Increase volume
-            case '+':
-                DPRINTLNF("Received Command: VOLUME +");
-                increaseVolume();
-                break;
-
-            // Decrease volume
-            case '-':
-                DPRINTLNF("Received Command: VOLUME -");
-                decreaseVolume();
-                break;
-
-            // Ignore newlines
-            case '\n':
-                break;
-
-            // Log invalid commands
-            default:
-                DPRINTF("Invalid command: ");
-                DPRINTLN(c);
-                break;
-        }
-    }
+    handleSerial();
     #endif
 
-    delay(100);
+    advanceTrack();
 }
 
 // ##################################
@@ -197,7 +131,7 @@ void playTogglePause() {
 
 // Skip forward
 void playNextTrack() {
-    if (currentTrack == currentAlbumTrackCount) {
+    if (currentTrack == currentAlbumTrackCount - 1) {
         return;
     }
 
@@ -226,6 +160,9 @@ void playAlbum() {
         return;
     }
 
+    resetPlayback();
+    isPlaying = true;
+
     DPRINTF("Playing album ");
     DPRINTLN(currentAlbum);
 
@@ -243,25 +180,62 @@ void playFile() {
     musicPlayer.startPlayingFile(path);
 }
 
+// Advance to the next track
+void advanceTrack()
+{
+    // Don't do anything if we are not playing
+    if (!isPlaying) {
+        return;
+    }
+
+    // Exit if we are currently playing or paused
+    if (musicPlayer.playingMusic || musicPlayer.paused()) {
+        return;
+    }
+
+    // Exit if we reached the last track
+    if (currentTrack == currentAlbumTrackCount - 1) {
+        resetPlayback();
+        DPRINTLNF("End of album reached");
+        return;
+    }
+
+    playNextTrack();
+}
+
+// Reset album playback
+void resetPlayback() {
+    isPlaying = false;
+    musicPlayer.stopPlaying();
+    currentTrack = 0;
+}
+
 // Detects the god mode sequence
 // Sequence: 1 - 2 - 4 - 8
-void detectGodMode(char c) {
+bool detectGodMode(char c) {
     if (gmflag == 0 && c == '1') {
         gmflag++;
+        return false;
     } else if (gmflag == 1 && c == '2') {
         gmflag++;
+        return false;
     } else if (gmflag == 2 && c == '4') {
         gmflag++;
+        return false;
     } else if (gmflag == 3 && c == '8') {
         setGodMode();
-    } else {
-        gmflag = 0;
+        return true;
     }
+
+    gmflag = 0;
+    return false;
 }
 
 // Selects god mode albums
 void setGodMode() {
     DPRINTLNF("Godmode enabled");
+
+    resetPlayback();
 
     musicPlayer.stopPlaying();
     musicPlayer.sineTest(150, 1000);
@@ -318,4 +292,79 @@ void loadAlbum() {
     }
 
     currentAlbumTrackCount = i;
+}
+
+// ##################################
+// Serial controls
+// ##################################
+void handleSerial() {
+    if (Serial.available()) {
+        char c = Serial.read();
+
+        switch (c) {
+            // Start playing
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                DPRINTF("Received Command: PLAY ");
+                DPRINTLN(c);
+                currentAlbum[2] = c;
+                if (!detectGodMode(c)) {
+                    playAlbum();
+                };
+                break;
+
+            // GOD mode !!!
+            case 'g':
+                DPRINTLNF("Received Command: God Mode");
+                setGodMode();
+                break;
+
+            // Toggle play/pause
+            case 'p':
+                DPRINTLNF("Received Command: TOGGLE PLAY/PAUSE");
+                playTogglePause();
+                break;
+
+            // Next track
+            case 'f':
+                DPRINTLNF("Received Command: NEXT TRACK");
+                playNextTrack();
+                break;
+
+            // Previous track
+            case 'b':
+                DPRINTLNF("Received Command: PREVIOUS TRACK");
+                playPreviousTrack();
+                break;
+
+            // Increase volume
+            case '+':
+                DPRINTLNF("Received Command: VOLUME +");
+                increaseVolume();
+                break;
+
+            // Decrease volume
+            case '-':
+                DPRINTLNF("Received Command: VOLUME -");
+                decreaseVolume();
+                break;
+
+            // Ignore newlines
+            case '\n':
+                break;
+
+            // Log invalid commands
+            default:
+                DPRINTF("Invalid command: ");
+                DPRINTLN(c);
+                break;
+        }
+    }
 }
